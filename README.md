56bcb023bf52aa1b751c4b52a8cefeb250f78027
ninohadwiger <ninohadwiger@yahoo.de>
2021-12-09 19:32:51 +0100

    aoc: use inquirer to create interactive command line

--- README.md
+++ README.md
@@ -4,3 +4,35 @@
 
 * [Day 1: Sonar Sweep](./day-01)
 * [Day 2: Dive!](./day-02)
+
+## Getting Started
+
+### Prerequisites
+
+* `npm`
+  ```test
+    npm install npm@latest -g
+  ```
+  
+### Installation
+
+Clone the repo and install the dependencies.
+
+
+1. Clone the repository
+    ```text
+    git clone https://github.com/ninohadwiger/2021-advent-of-code.git
+    cd 2021-advent-of-code
+    ```
+2. Install `npm` packages
+   ```text
+    npm install
+    ```
+   
+### Usage
+
+To run the application run the following
+
+```text
+npm start
+```
--- index.ts
+++ index.ts
@@ -1,42 +1,54 @@
 import {FileUtils} from "./utils/file.utils";
 import * as fs from "fs";
+import {prompt} from 'inquirer';
 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+const allFiles = fs.readdirSync('.');
+const availableDays = allFiles
+    .filter(dir => dir.startsWith('day'))
+    .map(dir => ({
+        name: 'Day ' + dir.slice(-2),
+        value: [parseInt(dir.slice(-2), 10)]
+    }));
 
-readline.question(`Solve challenge of day `, input => {
-    console.log();
+prompt([
+    {
+        type: 'list',
+        name: 'days',
+        message: 'Which challenge would you like to be solved?',
+        default: availableDays[availableDays.length - 1].value,
+        choices: [{name: 'All', value: availableDays.map(day => day.value[0])}, ...availableDays],
+    },
+]).then(x => {
 
-    const day = parseInt(input, 10);
-    const dayString = day.toString().padStart(2, '0');
+    const {days} = x;
 
-    // check if input is valid
-    if (Number.isNaN(day) || day < 1 || day > 25) {
-        console.error(`You provided an invalid day (${day}).`)
-        console.error(`The input must be between 1 and 25`);
-        process.exit(1);
+    for (let day of days) {
+
+        const dayString = day.toString().padStart(2, '0');
+
+        // check if input is valid
+        if (Number.isNaN(day) || day < 1 || day > 25) {
+            console.error(`You provided an invalid day (${day}).`)
+            console.error(`The input must be between 1 and 25`);
+            process.exit(1);
+        }
+
+        const srcFolder = `./day-${dayString}/src`;
+        const srcFile = `${srcFolder}/day-${dayString}`;
+
+        // check if day is already implemented
+        if (!fs.existsSync(srcFolder)) {
+            console.error(`Day ${day} is not implemented yet.`);
+            process.exit(1);
+        }
+
+        const callback = require(srcFile)['main'];
+
+        const assetFolder = `./day-${dayString}/assets`;
+        const inputFile = `${assetFolder}/data.in`;
+        const lines = FileUtils.getLinesOfFile(inputFile);
+
+        callback(lines, days);
     }
-
-
-    const srcFolder = `./day-${dayString}/src`;
-    const srcFile = `${srcFolder}/day-${dayString}`;
-
-    // check if day is already implemented
-    if (!fs.existsSync(srcFolder)) {
-        console.error(`Day ${day} is not implemented yet.`);
-        process.exit(1);
-    }
-
-    const callback = require(srcFile)['main'];
-
-    const assetFolder = `./day-${dayString}/assets`;
-    const inputFile = `${assetFolder}/data.in`;
-    const lines = FileUtils.getLinesOfFile(inputFile);
-
-    callback(lines, day);
-
-    readline.close();
 });
 
